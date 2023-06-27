191e8a3da0277d2bb79dd3ad080e36e30243c882
Joolanda <50619917+Joolanda@users.noreply.github.com>
2023-06-28 00:18:10 +0200

    styling buttons

---
+++ src/components/ImageShark.js
@@ -0,0 +1,73 @@
+const AWS = require('aws-sdk');
+// const sharp = require('sharp');
+
+// const s3 = new AWS.S3(); using S3Client instead, refactoring below
+const { S3Client, CopyObjectCommand } = require('@aws-sdk/client-s3')
+// , PutObjectCommand
+const DEST_BUCKET ='jols-bucket'
+const REGION ='us-east-1';
+const API_ACCESS = process.env.REACT_APP_ACCESS;
+const API_SECRET = process.env.REACT_APP_SECRET;
+
+AWS.config.update({
+  accessKeyId: API_ACCESS,
+  secretAccessKey: API_SECRET,
+  region: REGION,
+});
+
+const ImageShark = () => {
+  const handler = async (event) => {
+    // Read data from event object.
+    const region = event.Records[0].awsRegion
+    const sourceBucket = event.Records[0].s3.bucket.name
+    const sourceKey = event.Records[0].s3.object.key
+  
+    // let newSourceKey = `${sourceKey}-copy`
+    console.log('sourceKey: ', sourceKey);
+    // Instantiate a new S3 client.
+    const s3Client = new S3Client({
+      region: region,
+    })
+
+  /*   try { */
+      const copyObjectParams = {
+        Bucket: DEST_BUCKET,
+        Key: sourceKey,
+        CopySource: `${sourceBucket}/${sourceKey}`
+      };
+      console.log('copyObjectParams: ', copyObjectParams);
+      
+      const copyObjectResult = s3Client.send(new CopyObjectCommand(copyObjectParams));
+
+      // Execute object copy between buckets and return the result.
+      return s3Client.send(new CopyObjectCommand(copyObjectParams)),
+
+        s3Client.send(new CopyObjectCommand(copyObjectParams)),
+
+        console.log('copyObjectResult: ', copyObjectResult);
+  /*   } catch (error) {
+      console.log('error: ', error);
+      return error;
+    } */
+
+    
+};
+
+handler({
+  Records: [
+    {
+      awsRegion: "us-east-1",
+      s3: {
+        bucket: {
+          name: "jols-bucket",
+        },
+        object: {
+          key: "sourceKey",
+        },
+      },
+    },
+  ],
+});
+};
+
+export default ImageShark;
